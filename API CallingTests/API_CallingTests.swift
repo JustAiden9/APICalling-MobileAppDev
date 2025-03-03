4276778d471f940a854fc13e965aca6795c1dd56
Aiden Baker <aidenbak9@gmail.com>
2025-03-03 09:34:37 -0600

    Initial Commit

---
+++ API CallingTests/API_CallingTests.swift
@@ -0,0 +1,16 @@
+//
+//  API_CallingTests.swift
+//  API CallingTests
+//
+//  Created by Aiden Baker on 3/3/25.
+//
+
+import Testing
+
+struct API_CallingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
